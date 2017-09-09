ad272c19f75ba1b4614691a5d352528575516d39
Cmell <clmellinger@gmail.com>
2017-09-09 10:59:39 -0600

    Add sequence, repeat, and expandGrid functions, translated from R.

--- Util.js
+++ Util.js
@@ -277,8 +277,24 @@ function recycle (arr, num) {
   // arr should be an array to recycle.
   // num can be either an integer number to recycle to, or an array whose
   // length will be matched.
-  if (typeof num === 'object') {
+  if (Array.isArray(num)) {
     num = num.length;
+  }
+
+  if (!Array.isArray(arr)) {
+    if (typeof arr == "number") {
+      arr = [arr];
+    } else {
+      throw "arr must be an array or a number!";
+    }
+  }
+
+  if (arr.length < 1) {
+    throw "Can't recycle empty array.";
+  }
+
+  if (num < 0) {
+    throw "num must be 0 or greater.";
   }
 
   rep = Math.floor(num / arr.length);
@@ -320,3 +336,157 @@ function randomRecycle (arr, num) {
   }
   return tempArr;
 };
+
+function genSeq (start, end, by) {
+  // Generate a sequence of numbers and return it in an array. by defaults to 1
+  // Start and end must be supplied, or default to 0 and 1 respectively.
+  start = typeof start === "undefined" ? 0 : start;
+  end = typeof end === "undefined" ? 1 : end;
+  by = typeof by === "undefined" ? 1 : by;
+
+  var nextElement = start;
+  var retVal = [];
+
+  if (start < end) {
+    while (nextElement <= end) {
+      retVal.push(nextElement);
+      nextElement = nextElement + by;
+    }
+  } else if (end < start) {
+    while (nextElement >= end) {
+      retVal.push(nextElement);
+      nextElement = nextElement - by;
+    }
+  } else if (end == start) {
+    retVal = [start];
+  }
+
+  return(retVal);
+}
+
+function prod (arr) {
+  // Compute the product of all elements in an array.
+  var multiply = function (a, b) {
+    return(a * b);
+  }
+  var retVal = arr.reduce(multiply);
+  return(retVal);
+}
+
+function lengths (arr) {
+  // return an array containing the lenths of each element of arr.
+  var arrL = arr.length;
+  var retVal = [];
+  for (i = 0; i < arrL; i++) {
+    retVal.push(arr[i].length);
+  }
+  return (retVal);
+}
+
+function repInt (x, times) {
+  // Replicates each element of x 'times' times if times.length == x.length.
+  // If times is a number, then repeat the whole vector 'times' times.
+  if (!Array.isArray(x)) {
+    x = [x];
+  }
+
+
+
+  var i = 0;
+  var retArr = [];
+
+  if (Array.isArray(times)) {
+    if (times.length == x.length) {
+
+      var tempArr = [];
+      var num, val;
+      var a=0;
+
+      for (i = 0; i < x.length; i++) {
+        tempArr = [];
+        num = times[i];
+        val = x[i];
+        for (a = 0; a < num; a++) {
+          tempArr.push(val);
+        }
+        retArr = retArr.concat(tempArr);
+      }
+    } else {
+      throw "times and x must be same length if both are arrays."
+    }
+  }
+
+  if (typeof times == "number") {
+    for (i = 0; i < times; i++) {
+      retArr = retArr.concat(x);
+    }
+  }
+
+return(retArr);
+}
+
+
+function expandGrid (factorArr, format) {
+  // factorList should be an array of arrays. Each element should correspond
+  // to a factor, and the elements of each factor array should be the levels of
+  // the factor. This function returns an array of arrays. Each array in the
+  // returned array contains levels of the iteration of the expanded factors.
+  // Based on the R function expand.grid()
+  // format should be a character string of either "wide" or "long". If wide,
+  // (default) then the result is returned as an array of arrays, each of which
+  // has length equal to the number of factors, and represents a single
+  // combination. If set to anything else, the result is an array of length
+  // equal to the number
+  // of factors, and each element is an array containing the varying levels.
+
+  // default
+  format = typeof format == "undefined" ? "wide" : format;
+  var nargs = factorArr.length;
+  if (nargs == 1) {
+    factorArr = factorArr[0];
+    nargs = factorArr.length;
+  }
+  if (nargs == 0) {
+    return([]);
+  }
+  var x, nx, i, a, repFacTimes, whichElements, newX;
+  var cargs = [];
+  var repFac = 1;
+  var d = lengths(factorArr);
+  var oRep = prod(d);
+  if (oRep == 0) {
+    throw "All factors must have more than 0 levels."
+  } else {
+    for (i=0; i < nargs; i++) {
+      x = factorArr[i];
+      newX = [];
+      nx = x.length;
+      oRep = oRep / nx;
+      repFacTimes = repInt(repFac, nx);
+      whichElements = repInt(genSeq(0, nx-1), repFacTimes);
+      whichElements = repInt(whichElements, oRep);
+      for (a = 0; a < whichElements.length; a++) {
+        //debugger;
+        newX.push(x[whichElements[a]]);
+      }
+      cargs[i] = newX;
+      repFac = repFac * nx;
+    }
+  }
+// Put them all in the right format.
+
+if (format == "wide") {
+  var newCargs = [];
+  var tempArr = [];
+  for (i = 0; i < cargs[0].length; i++) {
+    tempArr = [];
+    for (a = 0; a < cargs.length; a++) {
+      tempArr[a] = cargs[a][i];
+    }
+    newCargs[i] = tempArr;
+  }
+  cargs = newCargs;
+}
+
+return(cargs);
+}
